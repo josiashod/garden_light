08ce2c0ad46355802666ae9b98d54a3cba41d40b
josiashod <djossoujosiashod@gmail.com>
2023-11-22 22:20:16 +0100

    Raining signal ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
+// number of drops created.
+var nbDrop = 858; 
+
+// function to generate a random number range.
+function randRange( minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
+// function to generate drops
+function createRain() {
+
+	for( i=1;i<nbDrop;i++) {
+	var dropLeft = randRange(0,2000);
+	var dropTop = randRange(-1000,1400);
+
+	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+    $('#drop'+i).css('left',dropLeft);
+    $('#drop'+i).css('top',dropTop);
+	}
+}
+// // Make it rain
+createRain();
+
 $(function() {
   const socket = io();
 
@@ -5,6 +28,11 @@ $(function() {
     document.documentElement.setAttribute("data-theme", theme);
 
     window.localStorage.setItem('theme', theme)
+  }
+
+  const changeWeather = (status) => {
+    document.documentElement.setAttribute("data-rain", status);
+    window.localStorage.setItem('rain', status)
   }
 
   $( document ).ready(function() {
@@ -17,11 +45,26 @@ $(function() {
 
     switchTo = currentTheme === 'light' ? 'dark' : 'light';
     changeTheme(switchTo);
-    socket.emit('changeLight', switchTo)
+    socket.emit('change light', switchTo)
+  });
+
+  clouds = document.querySelectorAll(".cloud")
+  clouds.forEach(el => {
+    el.addEventListener("click", function () {
+      currentTheme = document.documentElement.getAttribute("data-rain");
+      switchTo = currentTheme === 'off' ? 'on' : 'off';
+
+      changeWeather(switchTo);
+      socket.emit('make it rain', switchTo)
+    });
   });
 
   socket.on('theme changed', (theme) => {
     changeTheme(theme)
+  });
+
+  socket.on('weather changed', (weather) => {
+    changeWeather(weather)
   });
 
   socket.on('disconnect', () => {
--- server.js
+++ server.js
@@ -17,21 +17,30 @@ app.get('/', (req, res) => {
 //broadcasting
 let numUsers = 0;
 var theme = "dark";
+var rain = "water_stop"
 
 io.on('connection', (socket) => {
   let addedUser = false;
   
-  // when the client emits 'add user', this listens and executes
-  socket.on('changeLight', (value) => {
+  // when the client emits 'change light', this listens and executes
+  socket.on('change light', (value) => {
     theme = value
-    socket.emit('arduinoLED', theme);
+    socket.emit('led', theme);
     socket.broadcast.emit('theme changed', theme);
+  });
+
+  // when the client emits 'make it rain', this listens and executes
+  socket.on('make it rain', (value) => {
+    rain = value
+    socket.emit('water', rain);
+    socket.broadcast.emit('weather changed', rain);
   });
 
   socket.on('add user', (uuid) => {
     // we store the uuid in the socket session for this client
     socket.username = uuid;
-    io.to(uuid).emit('theme changed', theme)
+    socket.emit('theme changed', theme)
+    socket.emit('weather changed', rain)
     ++numUsers;
     addedUser = true;
   });
